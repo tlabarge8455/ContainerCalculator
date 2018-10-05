115c5c64ae618e01bf63687f498b5fa7b417b1dc
Tim LaBarge <tlabarge8455@gmail.com>
2018-10-05 11:18:35 -0400

    Working Basic Calculator

---
+++ ContainerCalculator/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainerCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContainerCalculator/wwwroot/js/calculator.js
@@ -0,0 +1,15 @@
+﻿function add() {
+    var num1 = getIntValueById('num1');
+    var num2 = getIntValueById('num2');
+    alert(num1 + num2);
+}
+
+function subtract() {
+    var num1 = getIntValueById('num1');
+    var num2 = getIntValueById('num2');
+    alert(num1 - num2);
+}
+
+function getIntValueById(id) {
+    return parseInt(document.getElementById(id).value)
+}
